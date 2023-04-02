8777ee7a5593434201942120b7041d2d356d2ee6
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-02 20:50:11 +0900

    追加

--- SFCymbolAppWithSwiftUI/ContentView.swift
+++ SFCymbolAppWithSwiftUI/ContentView.swift
@@ -6,21 +6,60 @@
 //
 
 import SwiftUI
+import SFUserFriendlySymbols
 
-struct ContentView: View {
+struct CatalogItem: View {
+    
+    private let symbol: SFSymbols
+    private let color: Color
+    private let backgroundColor: Color
+    private let baseLength: CGFloat
+    private let imageWidth: CGFloat
+    
+    init(symbol: SFSymbols, color: Color, backgroundColor: Color) {
+        self.symbol = symbol
+        self.color = color
+        self.backgroundColor = backgroundColor
+        baseLength = UIScreen.main.bounds.width * 0.25
+        imageWidth = baseLength * 0.5
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(alignment: .center) {
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(backgroundColor)
+                
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(.gray, lineWidth: 0.5)
+                
+                Image(symbol: symbol)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: imageWidth)
+                    .foregroundColor(color)
+            }
+            .frame(width: baseLength, height: baseLength)
+            
+            HStack {
+                Spacer()
+                
+                Text(symbol.rawValue)
+                    .frame(height: 35, alignment: .top)
+                    .multilineTextAlignment(.center)
+                    .font(.caption)
+                    .textSelection(.enabled)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CatalogItem(symbol: SFSymbols.bold, color: .black, backgroundColor: .white)
     }
 }
