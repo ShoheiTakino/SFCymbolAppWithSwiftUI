cf6445b51a02716b10cc70e4db4ca207b56d66c4
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-02 20:49:30 +0900

    ViewModel追加

---
+++ SFCymbolAppWithSwiftUI/ViewModel/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  SFCymbolAppWithSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/02.
+//
+
+import Foundation
+import SFUserFriendlySymbols
+//
+//class ViewModel: ObservableObject {
+//    @Published var text: String = ""
+//    private let allItems: [SFSymbols] = (0..<SFSymbols.allCases.count).map { "\($0)" }
+//    
+//    @Published var items: [String] = []
+//    
+//    func filter(by text: String) {
+//        items = text.isEmpty ? self.allItems : self.allItems.filter { $0.contains(text) }
+//    }
+//}
